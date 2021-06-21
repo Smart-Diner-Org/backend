8747e0c9dc53ad04d9f84b14b5681a7223e45c7b
Sharmiladevi N S <sharmiladevi17@gmail.com>
2021-06-21 13:47:20 +0530

    added the port number with the sequlize connection

--- config/database.js
+++ config/database.js
@@ -7,6 +7,7 @@ const operatorsAliases = {
 
 module.exports = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
 	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
 	dialect: process.env.SEQUELIZE_DIALECT,
 	pool: {
 		max: 5,
