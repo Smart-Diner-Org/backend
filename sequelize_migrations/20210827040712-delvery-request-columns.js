d744f52b375e0860439fdc1f4fc81b5ae7c2c660
Nandhini Siva <nandhinisiva1237@gmail.com>
2021-08-27 10:05:45 +0530

    added columns to delivery-request table

---
+++ sequelize_migrations/20210827040712-delvery-request-columns.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+up: function (queryInterface, Sequelize) {
+return Promise.all([
+  queryInterface.addColumn('delivery_requests', 'delivery_type', {
+      type: Sequelize.BIGINT,
+      notNull: false
+  }),
+  queryInterface.addColumn('delivery_requests', 'scheduled_data_time', {
+    type:  Sequelize.DATE,
+    notNull: false
+  }),
+  queryInterface.addColumn('delivery_requests', 'request_id', {
+      type: Sequelize.TEXT,
+      notNull: true,
+  }),
+  queryInterface.addColumn('delivery_requests', 'referrence_id', {
+      type: Sequelize.TEXT,
+      notNull: true,
+  }),
+  queryInterface.addColumn('delivery_requests', 'task_id', {
+      type: Sequelize.TEXT,
+      notNull: true,
+  }),
+  queryInterface.addColumn('delivery_requests', 'pick_up_details', {
+      type: Sequelize.JSON,
+      notNull: true,
+  }),
+  queryInterface.addColumn('delivery_requests', 'drop_details', {
+      type: Sequelize.JSON,
+      notNull: true,
+  })
+  ])
+ }
+};
